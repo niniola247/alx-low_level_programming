ce70e1e26154c4e4c027f297625f3e4c830fc3d6
niniola eniola <niniolaarafat24@gmail.com>
2022-10-16 08:26:00 -0700

    ok

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reversed alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
