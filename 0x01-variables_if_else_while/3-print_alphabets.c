f93a9105e4070651060f971154f05479f7ba8d0a
niniola eniola <niniolaarafat24@gmail.com>
2022-10-16 08:01:18 -0700

    ok

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lower and upper case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	for (l = 'A'; l <= 'Z'; l++)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
