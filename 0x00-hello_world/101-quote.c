b9e19018e344e73601ddc761d6525b5480e86ebd
niniola eniola <niniolaarafat24@gmail.com>
2022-10-16 07:23:03 -0700

    ok

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print a string to the standard error
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, err, 59);
+
+	return (1);
+}
