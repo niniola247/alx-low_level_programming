e649ba7ad4bfd07ad4b543870ef30cf8c7e28cbb
niniola eniola <niniolaarafat24@gmail.com>
2022-10-24 10:31:24 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints given string in reverse
+ * @s: passed pointer argument for string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
