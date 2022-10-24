d14e63a364b6aa9fc08a53dcd46fbede12907739
niniola eniola <niniolaarafat24@gmail.com>
2022-10-24 11:10:00 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a given string
+ * @str: passed string pointer
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
